f96d97040fdea922b8a2cbe2a53609f0d1830ba2
unknown <pikalot@hotmail.com>
2025-12-24 12:27:13 -0800

    Added playwright

---
+++ README.md
@@ -0,0 +1,4 @@
+# To install other browswer
+```
+playwright install
+```
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world!")
+from playwright.sync_api import sync_playwright
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads .env into environment variables
+
+def screen_web(url):
+    print("do something")
+    browser_name = os.getenv('BROWSER', 'chrome')
+    print("browser_name: " + browser_name)
+    with sync_playwright() as p:
+        browser = getattr(p, browser_name).launch(headless=False)
+        page = browser.new_page()
+        page.goto(url)
+        print("goto url: " + url)
+        content = page.content()
+        browser.close()
+        return content
+    
+def main():
+    url = "https://playwright.dev/"
+    html_content = screen_web(url)
+    print(html_content)
+
+if __name__ == "__main__":
+    main()
